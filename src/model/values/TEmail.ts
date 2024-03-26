266ed33c38c253e60fee6c61aef8eb9da68edbaa
mgn901 <mgn901+dev@gmail.com>
2024-03-26 07:36:12 +0000

    add: Nominal primitives used in model

---
+++ src/model/values/TEmail.ts
@@ -0,0 +1,25 @@
+import type { TNominalPrimitive } from '../../utils/primitives/TNominalPrimitive.ts';
+
+const emailTypeSymbol = Symbol('emailTypeSymbol');
+
+/**
+ * Eメールアドレスを表す公称プリミティブ型。
+ */
+export type TEmail = TNominalPrimitive<string, typeof emailTypeSymbol>;
+
+/**
+ * 文字列がEメールアドレスであるかを判定する関数。
+ *
+ * 文字列がHTML仕様の「valid e-mail address」に準拠するかを判定する。
+ * 「valid email-address」はRFC 5322におけるEメールアドレスの定義とは異なることに注意する。
+ *
+ * 参照: https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
+ *
+ * @param value 判定対象の文字列。
+ * @returns `value`がEメールアドレスであれば`true`を返し、`value`を`TEmail`とみなす。そうでなければ`false`を返す。
+ */
+export const isEmail = (value: string): value is TEmail => {
+  const regexp =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  return regexp.test(value);
+};
---
+++ src/model/values/TTitleForUrl.ts
@@ -0,0 +1,23 @@
+import type { TNominalPrimitive } from '../../utils/primitives/TNominalPrimitive.ts';
+
+const titleForUrlTypeSymbol = Symbol('titleForUrlTypeSymbol');
+
+/**
+ * タイトルのURL用表現を表す公称プリミティブ型。
+ * 制御文字以外の文字列、英字は小文字のみから成る。ただし、先頭と末尾が空白以外の文字列である必要がある。
+ */
+export type TTitleForUrl = TNominalPrimitive<string, typeof titleForUrlTypeSymbol>;
+
+/**
+ * 文字列がタイトルのURL用表現になりうるかを判定する関数。
+ *
+ * 文字列が制御文字以外の文字列、英字は小文字のみから成り、先頭と末尾が空白以外の文字であるかを判定する。
+ *
+ * @param value 判定対象の文字列。
+ * @returns `value`がタイトルのURL用表現になりうれば`true`を返し、`value`を`TTitleForUrl`とみなす。そうでなければ`false`を返す。
+ */
+export const isTitleForUrl = (value: string): value is TTitleForUrl => {
+  // biome-ignore lint/suspicious/noControlCharactersInRegex: 制御文字を取り除く。
+  const regexp = /^[^\x00-\x1F\x7FA-Z]+$/;
+  return value.trim() === value && regexp.test(value);
+};
