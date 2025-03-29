827236e96ab5ab9f37b5b3d17928b86834a0fe11
mgn901 <mgn901+dev@gmail.com>
2025-03-29 16:09:48 +0000

    fix: TTitle -> Title

--- src/model/values.ts
+++ src/model/values.ts
@@ -78,7 +78,7 @@ const titleTypeSymbol = Symbol('titleTypeSymbol');
  * タイトルを表す公称プリミティブ型。
  * 制御文字以外の文字列から成る。ただし、先頭と末尾が空白以外の文字列である必要がある。
  */
-export type TTitle = NominalPrimitive<string, typeof titleTypeSymbol>;
+export type Title = NominalPrimitive<string, typeof titleTypeSymbol>;
 
 /**
  * 文字列がタイトルになりうるかを判定する関数。
@@ -88,7 +88,7 @@ export type TTitle = NominalPrimitive<string, typeof titleTypeSymbol>;
  * @param value 判定対象の文字列。
  * @returns `value`がタイトルになりうれば`true`を返し、`value`を{@linkcode Title}とみなす。そうでなければ`false`を返す。
  */
-export const isTitle = (value: string): value is TTitle => {
+export const isTitle = (value: string): value is Title => {
   // biome-ignore lint/suspicious/noControlCharactersInRegex: 制御文字を取り除く。
   const regexp = /^[^\x00-\x1F\x7F]+$/;
   return value.trim() === value && regexp.test(value);
